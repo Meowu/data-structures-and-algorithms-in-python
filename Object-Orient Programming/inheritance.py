a2915e3cab9b87a904a9fd860c30750a7fb2e84d
Meowu <474384902@qq.com>
2018-09-30 14:16:52 +0800

    add inheritance

--- Object-Orient Programming/inheritance.py
+++ Object-Orient Programming/inheritance.py
@@ -2,4 +2,31 @@ from .class_definition import CreditCard
 
 
 class PredatoryCreditCard(CreditCard):
-    pass
+    """An extension to CreditCard that compounds interest and fees."""
+
+    def __init__(self, customer, bank, account, limit, apr):
+        """Create a new predatory credit card instance.
+        The initial balance is zero.
+        
+        """
+        super().__init__(customer, bank, account, limit)
+        self._apr = apr
+
+    def charge(self, price):
+        """
+        Charge given price to the card, assuming sufficient credit limit.
+        :param price: 
+        :return: Return True if charge was processed.
+        :return: False and assess $5 fee if charge if denied.
+        """
+        success = super().charge(price)
+        if not success:
+            self._balance += 5
+        return success
+
+    def process_month(self):
+        """Assess monthly interest on outstanding balance."""
+
+        if self._balance > 0:
+            monthly_factor = pow(1 + self._apr, 1 / 12)
+            self._balance *= monthly_factor
