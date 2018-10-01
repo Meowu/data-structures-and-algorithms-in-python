78f4bc6fbe567e92d8d91abbb1bf54e12bfd69ec
wuchengjie <474384902@qq.com>
2018-10-01 15:50:52 +0800

    owner person of collections.Sequence.

---
+++ Object-Orient Programming/abstract_base_class.py
@@ -0,0 +1,36 @@
+#!/usr/env/bin python3
+
+from abc import ABCMeta, abstractmethod
+
+class Sequence(metaclass=ABCMeta):
+    """A version of collections.Sequence abstract base class."""
+
+    @abstractmethod
+    def __len__(self):
+        """Return the length of  Sequence."""
+
+    @abstractmethod
+    def __getitem__(self, i):
+        """Return the element at index i of Sequence."""
+
+    def __contains__(self, val):
+        """Return True if val in Sequence else return False."""
+        for i in range(len(self)):
+            if self[i] == val:
+                return True
+        return False
+
+    def index(self, val):
+        """Return leftmost index at which val is found or raise ValueError."""
+        for i in range(len(self)):
+            if self[i] == val:
+                return i
+        raise ValueError("val is not in sequence."""
+
+    def count(self, val):
+        """Return the number of val in sequence."""
+        count = 0
+        for i in range(len(self)):
+            if self[i] == val:
+                count++
+        return count
