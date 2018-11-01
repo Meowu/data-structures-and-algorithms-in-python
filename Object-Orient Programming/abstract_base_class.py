c6f0a7ab7aef13af1bf7d0954efa43384f96299f
Meowu <474384902@qq.com>
2018-11-01 09:32:41 +0800

    fixed: count++ => count =+ 1

--- Object-Orient Programming/abstract_base_class.py
+++ Object-Orient Programming/abstract_base_class.py
@@ -32,5 +32,5 @@ class Sequence(metaclass=ABCMeta):
         count = 0
         for i in range(len(self)):
             if self[i] == val:
-                count++
+                count += 1
         return count
