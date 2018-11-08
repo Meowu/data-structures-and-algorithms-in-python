717d69d82ca7c8b594cc6772696b5366ed13b7ff
Meowu <474384902@qq.com>
2018-11-08 20:46:29 +0800

    feature: disk_usage method.

---
+++ Recursion/disk_usage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+import os
+
+
+def disk_usage(path):
+    total = os.path.getsize(path)
+    if os.path.isdir(path):
+        for item in os.listdir(path):
+            total += disk_usage(os.path.join(path, item))
+
+    return total
