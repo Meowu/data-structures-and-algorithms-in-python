c8b3ff61604d72b5fa01c21ddf349cf1ac308a98
Meowu <474384902@qq.com>
2018-12-16 17:59:39 +0800

    module: implement a array based stack using Python list as internal.

---
+++ Stacks-Queues-Deques/array_stack.py
@@ -0,0 +1,30 @@
+
+class Empty(Exception):
+
+    pass
+
+
+class ArrayStack:
+
+    def __init__(self):
+
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def top(self):
+        if self.is_empty():
+            raise Empty('Stack is empty.')
+        return self._data[-1]
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty('Stack is empty.')
+        return self._data.pop()
+
+    def push(self, obj):
+        self._data.append(obj)
