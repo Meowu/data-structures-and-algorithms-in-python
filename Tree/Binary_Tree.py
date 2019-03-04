c051f9c50f9f38051c364796006e7f17beba2f43
Meowu <474384902@qq.com>
2019-03-04 19:41:11 +0800

    fix: clear tree ADT.

--- Tree/Binary_Tree.py
+++ Tree/Binary_Tree.py
@@ -2,6 +2,14 @@ from .Tree import Tree
 
 
 class BinaryTree(Tree):
+    """
+        A binary tree is an ordered tree with the following properties:
+        1. Every node has at most two children.
+        2. Each child node is labeled as being either a left child or a right child.
+        3. A left child precedes a right child in the order of children of a node.
+        
+        A binary tree is proper if `each node` has either zero or two children. 也就是
+    """
 
     def right(self, p):
         raise NotImplementedError('must be implemented by subclass ')
@@ -19,7 +27,7 @@ class BinaryTree(Tree):
             return self.right(parent)
 
     def children(self, p):
-        if self.left(p) is not None:
+        if self.left(p) is not None: # 遍历的时候从左到右。
             yield self.left(p)
         if self.right(p) is not None:
             yield self.right(p)
